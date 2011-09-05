2b53e5856c5451b4d5c9fc61931c8b8beda52934
Dave Brotherstone <davegb@pobox.com>
2011-09-05 22:09:02 +0000

    Welcome view, to present some helpful options to the new arrivee

--- app/classes/controller/welcome.php
+++ app/classes/controller/welcome.php
@@ -1,10 +1,15 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Controller_Welcome extends Controller {
-
+	public function before()
+	{
+		parent::before();	
+	}
+	
 	public function action_index()
 	{
-		$this->request->response = 'hello, world!';
+		# $this->request->response = 'hello, world!';
+		$this->body = new View('welcome/index');		
 	}
 
 } // End Welcome
