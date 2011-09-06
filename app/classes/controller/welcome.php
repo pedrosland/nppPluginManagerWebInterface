4511f5f3a8a0b4cd87e389fed8012da4f3aed0b1
Pedro Sland <pedro_sland@hotmail.com>
2011-09-06 19:59:53 +0100

    Tidied welcome controller and view.

--- app/classes/controller/welcome.php
+++ app/classes/controller/welcome.php
@@ -1,14 +1,9 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Controller_Welcome extends Controller {
-	public function before()
-	{
-		parent::before();	
-	}
 	
 	public function action_index()
 	{
-		# $this->request->response = 'hello, world!';
 		$this->body = new View('welcome/index');		
 	}
 
--- app/views/welcome/index.php
+++ app/views/welcome/index.php
@@ -1,11 +1,7 @@
-<br/>
 <h1>Welcome to Notepad++ Plugins Respository</h1>
-<br/>
-If you have a username and password already, then you can login above.
-<br/><br/>
-To register for an account, click <a href="users/register">here</a>.
-<br/><br/>
-To watch a quick video of how to enter or update your plugin on the site,
-click <a href="http://youtube.com" target="_blank">here</a>
+<p>If you have a username and password already, then you can login above.</p>
+<p>To register for an account, click <a href="users/register">here</a>.</p>
+<p>To watch a quick video of how to enter or update your plugin on the site,
+click <a href="http://youtube.com" target="_blank">here</a>.</p>
 
 
