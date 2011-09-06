0d3a05dfabe2bc790e98fbff60e2b69552109ca8
Dave Brotherstone <davegb@pobox.com>
2011-09-06 20:46:52 +0000

    Added comment about documentation coming soon

--- app/views/welcome/index.php
+++ app/views/welcome/index.php
@@ -5,7 +5,5 @@ If you have a username and password already, then you can login above.
 <br/><br/>
 To register for an account, click <a href="users/register">here</a>.
 <br/><br/>
-To watch a quick video of how to enter or update your plugin on the site,
-click <a href="http://youtube.com" target="_blank">here</a>
-
+Documentation and possibly a quick video will be coming in the next few days.
 
